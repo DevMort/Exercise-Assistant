c5de857789690792c70203427b7982cc2a5beace
empyreum <empyreum@protonmail.com>
2021-04-26 20:33:21 +0800

    Costumize and add what exercise you want

--- Scripts/Exercise.gd
+++ Scripts/Exercise.gd
@@ -2,10 +2,24 @@ extends MarginContainer
 
 onready var button = $CenterContainer/Button
 onready var panel = $CanvasLayer/MarginContainer/Panel
+onready var confirm = $CanvasLayer/MarginContainer/Panel/MarginContainer/VBoxContainer/MarginContainer/CenterContainer/confirm
+onready var name_input = $CanvasLayer/MarginContainer/Panel/MarginContainer/VBoxContainer/name_input
+onready var notes_input = $CanvasLayer/MarginContainer/Panel/MarginContainer/VBoxContainer/notes_input
+
+var exercise_name : String = ""
+var notes : String = ""
 
 func _ready(): 
 	button.connect("pressed", self, "modify_exercise")
-	panel.hide()
+	confirm.connect("pressed", self, "confirm")
+	panel.get_parent().hide()
 
-func modify_exercise() -> void:
-	panel.show()
+func modify_exercise() -> void: 
+	button.disabled = true
+	panel.get_parent().show()
+func confirm() -> void:
+	exercise_name = name_input.text
+	notes = notes_input.text
+	 
+	panel.get_parent().hide()
+	button.disabled = false
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,9 +5,13 @@ export (PackedScene) var exercise_scene
 onready var add_exercise_button = $VBoxContainer/MarginContainer/HBoxContainer/add_exerise
 onready var grid = $VBoxContainer/MarginContainer2/GridContainer
 
+var ex_count : int = 0
+
 func _ready():
 	add_exercise_button.connect("pressed", self, "add_exercise")
 
 func add_exercise() -> void:
 	var e = exercise_scene.instance()
 	grid.add_child(e)
+	ex_count += 1
+	e.button.text = String(ex_count)
