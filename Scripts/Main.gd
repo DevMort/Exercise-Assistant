e474fde06da2e682517e18d64414b807ccb78aa9
empyreum <empyreum@protonmail.com>
2021-04-26 20:45:18 +0800

    Added rest

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,17 +1,28 @@
 extends Control
 
 export (PackedScene) var exercise_scene
+export (PackedScene) var rest_scene
 
 onready var add_exercise_button = $VBoxContainer/MarginContainer/HBoxContainer/add_exerise
+onready var add_rest_button = $VBoxContainer/MarginContainer/HBoxContainer/add_rest
 onready var grid = $VBoxContainer/MarginContainer2/GridContainer
+onready var start_button = $VBoxContainer/MarginContainer3/CenterContainer/start
 
 var ex_count : int = 0
+var re_count : int = 0
 
 func _ready():
 	add_exercise_button.connect("pressed", self, "add_exercise")
+	add_rest_button.connect("pressed", self, "add_rest")
 
 func add_exercise() -> void:
 	var e = exercise_scene.instance()
 	grid.add_child(e)
 	ex_count += 1
 	e.button.text = String(ex_count)
+
+func add_rest() -> void:
+	var r = rest_scene.instance()
+	grid.add_child(r)
+	re_count += 1
+	r.button.text = String(re_count)
---
+++ Scripts/Rest.gd
@@ -0,0 +1,25 @@
+extends MarginContainer
+
+onready var button = $CenterContainer/Button
+onready var panel = $CanvasLayer/MarginContainer/Panel
+onready var confirm = $CanvasLayer/MarginContainer/Panel/MarginContainer/VBoxContainer/MarginContainer/CenterContainer/confirm
+onready var minutes_input = $CanvasLayer/MarginContainer/Panel/MarginContainer/VBoxContainer/HBoxContainer/minutes
+onready var seconds_input = $CanvasLayer/MarginContainer/Panel/MarginContainer/VBoxContainer/HBoxContainer/seconds
+
+var minutes : String = ""
+var seconds : String = ""
+
+func _ready(): 
+	button.connect("pressed", self, "modify_exercise")
+	confirm.connect("pressed", self, "confirm")
+	panel.get_parent().hide()
+
+func modify_exercise() -> void: 
+	button.disabled = true
+	panel.get_parent().show()
+func confirm() -> void:
+	minutes = minutes_input.text
+	seconds = seconds_input.text
+	 
+	panel.get_parent().hide()
+	button.disabled = false
